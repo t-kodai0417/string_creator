dfd862fbb384ccf09d3b7e3c10a8a37a303c54e1
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-02-25 14:56:27 +0900

    Update stringpy.py

--- stringpy.py
+++ stringpy.py
@@ -1,6 +1,4 @@
 import random
 import string
 def create(l):
-    global data
-    data=(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(l)))
-
+    return(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(l)))
